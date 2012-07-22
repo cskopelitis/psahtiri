d8e131448fe367afa3e45383c33e0b796bd29bd0
cskopelitis <christos.skopelitis@gmail.com>
2012-07-22 12:34:24 +0300

    remove playlist item: v1

--- scripts/playlistctrl.js
+++ scripts/playlistctrl.js
@@ -7,18 +7,27 @@ Playlist.add = function(title, embedUrl) {
 		title : title,
 		embedUrl : embedUrl
 	};
-	Playlist.items.push(playlistItem);
-	Playlist.drawItem(playlistItem);
+	itemIndex = Playlist.items.push(playlistItem);
+	Playlist.drawItem(itemIndex, playlistItem);
 };
 
-Playlist.drawItem = function(playlistItem) {
-	// var itemIndex = Playlist.items.indexOf(playlistItem);
-	html = '<tr>';
+Playlist.draw = function() {
+	$('#playlist tr').remove();
+	$('#playlist').append('<tr><td><H2>Playlist</H2></td></tr>');
+
+	$.each(Playlist.items, function(itemIndex, item) {
+		Playlist.drawItem(itemIndex, item);
+	});
+};
+
+Playlist.drawItem = function(itemIndex, playlistItem) {
+	html = '<tr id="item' + itemIndex + '">';
 	html += '<td rowspan="2"><iframe width="120px" height="90px" src="'
 			+ playlistItem.embedUrl
 			+ '" frameborder="0" mozallowfullscreen allowFullScreen"></iframe></td>';
 	html += '<td style="text-align:right;">';
-	html += '<img style="cursor:pointer" src="images/trash.gif" onclick="alert(\'delete this\')";/>';
+	html += '<img style="cursor:pointer" src="images/trash.gif" onclick="Playlist.remove('
+			+ itemIndex + ');"/>';
 	html += '</td>';
 	html += '</tr><tr>';
 	html += '<td style="border-bottom: 1px solid #9AC336">'
@@ -27,4 +36,9 @@ Playlist.drawItem = function(playlistItem) {
 	html += '</tr>';
 
 	$('#playlist').append(html);
+};
+
+Playlist.remove = function(itemIndex) {
+	Playlist.items.splice(itemIndex, 1);
+	Playlist.draw();
 };
